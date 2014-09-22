8cddd1710e64158fb1cb9e8a23eb6afcac683a18
utv <k9amata@gmail.com>
2014-09-22 14:41:18 +0700

    commit for removing this file later

--- src/main/scala/code/snippet/SinglePageForm.scala
+++ src/main/scala/code/snippet/SinglePageForm.scala
@@ -2,7 +2,7 @@ package code.snippet
 
 import net.liftweb.http._
 
-class SinglePageForm extends LiftScreen {
+class UploadScreen extends LiftScreen {
   val from = field("E-mail", "", "placeholder" -> "Enter your e-mail")
   val subject = field("Subject", "", "placeholder" -> "Enter the subject of your message")
   val body = field("Message", "", "placeholder" -> "Enter your message")
