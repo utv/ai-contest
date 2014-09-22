f338d84c3702c19d7ebbe2103be28bedb7cd252e
utv <k9amata@gmail.com>
2014-09-22 22:28:43 +0700

    add upload field to LiftScreen

--- src/main/scala/code/snippet/UploadScreen.scala
+++ src/main/scala/code/snippet/UploadScreen.scala
@@ -1,16 +1,27 @@
 package code.snippet
 
 import net.liftweb.http._
+import net.liftweb.util._
+
+// import net.liftweb._
+// import http._
+// import common._
+// import scala.xml._
 
 class UploadScreen extends LiftScreen {
-  val from = field("E-mail", "", "placeholder" -> "Enter your e-mail")
+  /*val from = field("E-mail", "", "placeholder" -> "Enter your e-mail")
   val subject = field("Subject", "", "placeholder" -> "Enter the subject of your message")
-  val body = field("Message", "", "placeholder" -> "Enter your message")
+  val body = field("Message", "", "placeholder" -> "Enter your message")*/
+  override protected def hasUploadField = true
 
-  override def finishButton = <button>Save</button>
+  val name = field("Name", "", "placeholder" -> "Name of new game")
+  val file = makeField[Array[Byte], Nothing]("File", new Array[Byte](0),
+    field => SHtml.fileUpload(fph => field.set(fph.file)),NothingOtherValueInitializer)
+  
+  // override def finishButton = <button>Save</button>
 
-  protected def finish() {
-    S.notice("form submitted")
+  def finish() {
+    S.notice("Thanks for uploading a file of " + file.get.length + " bytes ")
     
   }
 }
