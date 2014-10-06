0e3842a27d40e73d4100ab90a0eeac28a0a0ef3c
utv <k9amata@gmail.com>
2014-10-06 21:36:45 +0700

    delete zip file after adding to DB

--- src/main/scala/code/snippet/Db.scala
+++ src/main/scala/code/snippet/Db.scala
@@ -5,9 +5,16 @@ import shapeless._
 import code.model.User
 
 object Db {
-	val myFirstname = 
+  val myFirstname = 
     db withSession {
-      sql("select firstname from users where lastname=?")apply(Some("Dee"))
+      sql("select firstname from users where lastname=?")apply(Some("doe"))
     }
+
+  def addGame(gameName: String) = {
+    db withTransaction {
+      sql("insert into games (creator_id, name) values (?,?)")apply(
+        Some(User.currentUserId.get.toInt), Some(gameName))
+    }
+  }
 
 }
--- src/main/scala/code/snippet/UploadScreen.scala
+++ src/main/scala/code/snippet/UploadScreen.scala
@@ -3,6 +3,8 @@ package code.snippet
 // import net.liftweb.http._
 // import net.liftweb.util._
 import java.io.{File, FileOutputStream}
+import java.nio.file.{Files, Path, Paths}
+
 
 import net.liftweb._
 import http._
@@ -16,21 +18,18 @@ class UploadScreen extends LiftScreen {
   
   var upload : Box[FileParamHolder] = Empty
   val urlAfterSubmission = "/static/index"
-  val filePath = "games"
   
   // Fields on LiftScreen
-  val name = field("Name", "", "placeholder" -> "Name of new game")
-  val file = makeField[Array[Byte], Nothing]("File", new Array[Byte](0),
-    field => SHtml.fileUpload(fph => upload = Full(fph)),NothingOtherValueInitializer)
+  val gameName = field("Name", "", "placeholder" -> "Name of new game")
+  val uploadFile = makeField[Array[Byte], Nothing]("File", new Array[Byte](0),
+    field => SHtml.fileUpload(fph => upload = Full(fph)), NothingOtherValueInitializer)
   
   // override def finishButton = <button>Save</button>
 
-  def unzipFile(source: String) = {
-    // val source = "src/main/webapp/images/test.zip"
-    // val destination = "games"
+  def unzipFile(source: String, dest: String) = {
     try { 
       val zipFile = new ZipFile(source);
-      zipFile.extractAll(filePath);
+      zipFile.extractAll(dest);
     } catch {
       case e: Exception => "Unable to unzip!!"
     }
@@ -38,12 +37,19 @@ class UploadScreen extends LiftScreen {
 
   def processUpload() = upload match {
     case Full(FileParamHolder(_, mimeType, fileName, file)) => 
-      val oFile = new File(filePath, fileName)
-      val output = new FileOutputStream(oFile)
-      output.write(file)
-      output.close()
-      unzipFile(oFile.getAbsolutePath())
-      
+      val gameDir = "games"
+      try { 
+        val oFile = new File(gameDir, fileName)
+        val output = new FileOutputStream(oFile)
+        output.write(file)
+        output.close()
+        
+        unzipFile(oFile.getAbsolutePath(), gameDir)
+        Db.addGame(gameName)
+        oFile.delete()
+      } catch {
+        case e: Exception => e.printStackTrace; println(e)
+      }
       S.notice("Thanks for the upload")
     case _ => println("No file?")
   }
