19ec8738e5a49bfbbea501ab6c2f92d7fb2da1ea
utv <k9amata@gmail.com>
2014-11-11 15:01:44 +0700

    back to use sqltyped

--- src/main/scala/code/snippet/Games.scala
+++ src/main/scala/code/snippet/Games.scala
@@ -5,26 +5,28 @@ import net.liftweb.mapper._
 
 class Games {
 
-  val (fieldNames: List[String], fieldValues: List[List[String]]) = 
+  /*val (fieldNames: List[String], fieldValues: List[List[String]]) = 
     DB.runQuery("select games.id, games.name, count(tourn_gameindex.tourn_id) as numOfTournament " +
-        "from games left join tourn_gameindex on games.id = tourn_gameindex.gameid group by games.id, games.name")
+        "from games left join tourn_gameindex on games.id = tourn_gameindex.gameid group by games.id, games.name")*/
 
   // "tbody tr *" #> fieldValues.map { values => "td *" #> values }
 
-  // val listOfGames = Db.listGame
-
-  def render = {
+  /*def render = {
     "tbody tr *" #> fieldValues.map( values => {
       "a [href]" #> appendParams("tournaments", Seq("id" -> values(0))) &
       "a *" #> values(1) &
       "@numOfTournament *" #> values(2) }
     )
+  }*/
 
-    /*"tbody tr *" #> 
+  val listOfGames = Db.listGame
+  
+  def render = {
+    "tbody tr *" #> 
       listOfGames.map(values => { 
         "a *" #> values.get("name")  &
         "a [href]" #> appendParams("tournaments", Seq("id" -> values.get("id").toString)) & // values.get("id") &
         "@numOfTournament *" #> values.get("numOfTournament")
-      })*/
+      })
   }
 }
